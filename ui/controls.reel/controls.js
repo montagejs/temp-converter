6df996d94991053956a5542512f0019805699b99
simurai <simulus@gmail.com>
2013-09-23 22:51:00 +0900

    Split main into header and controls reel

---
+++ ui/controls.reel/controls.js
@@ -0,0 +1,17 @@
+/**
+ * @module ./controls.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Controls
+ * @extends Component
+ */
+exports.Controls = Component.specialize(/** @lends Controls# */ {
+    constructor: {
+        value: function Controls() {
+            this.super();
+        }
+    }
+});
